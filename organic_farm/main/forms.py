b2f76f5ac1a8254e7202e5f76dd5d8cf4e7820cd
SQCodes <paulsqmagadi@gmail.com>
2024-08-07 23:55:14 +0300

    new file:   organic_farm/db.sqlite3 	new file:   organic_farm/main/__init__.py 	new file:   organic_farm/main/__pycache__/__init__.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/admin.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/apps.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/forms.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/managers.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/models.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/urls.cpython-311.pyc 	new file:   organic_farm/main/__pycache__/views.cpython-311.pyc 	new file:   organic_farm/main/admin.py 	new file:   organic_farm/main/apps.py 	new file:   organic_farm/main/forms.py 	new file:   organic_farm/main/managers.py 	new file:   organic_farm/main/migrations/0001_initial.py 	new file:   organic_farm/main/migrations/__init__.py 	new file:   organic_farm/main/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   organic_farm/main/migrations/__pycache__/__init__.cpython-311.pyc 	new file:   organic_farm/main/models.py 	new file:   organic_farm/main/templates/main/index.html 	new file:   organic_farm/main/tests.py 	new file:   organic_farm/main/urls.py 	new file:   organic_farm/main/views.py 	new file:   organic_farm/manage.py 	new file:   organic_farm/organic_farm/__init__.py 	new file:   organic_farm/organic_farm/__pycache__/__init__.cpython-311.pyc 	new file:   organic_farm/organic_farm/__pycache__/settings.cpython-311.pyc 	new file:   organic_farm/organic_farm/__pycache__/urls.cpython-311.pyc 	new file:   organic_farm/organic_farm/__pycache__/wsgi.cpython-311.pyc 	new file:   organic_farm/organic_farm/asgi.py 	new file:   organic_farm/organic_farm/settings.py 	new file:   organic_farm/organic_farm/urls.py 	new file:   organic_farm/organic_farm/wsgi.py 	new file:   organic_farm/static/css/main.css

---
+++ organic_farm/main/forms.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django import forms
+from .models import CustomUser
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
+
+
+class CustomUserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+class UpdateUserForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email']
+
+
+class UpdateUserPassword(PasswordChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ['new_password1', 'new_password2']
